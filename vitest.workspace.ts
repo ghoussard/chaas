d232c704b92c00173a0b9e15504aede7fea7c59f
Grégoire Houssard <ghoussard@protonmail.com>
2024-10-07 22:42:28 +0200

    Install vitest

---
+++ vitest.workspace.ts
@@ -0,0 +1,20 @@
+import {defineWorkspace} from 'vitest/config';
+
+export default defineWorkspace([
+  {
+    extends: './vite.config.ts',
+    test: {
+      name: 'unit',
+      include: ['**/*.unit.test.ts(x)'],
+      environment: 'jsdom',
+    },
+  },
+  {
+    extends: './vite.config.ts',
+    test: {
+      name: 'integration',
+      include: ['**/*.integration.test.ts(x)'],
+      environment: 'jsdom',
+    },
+  },
+]);
