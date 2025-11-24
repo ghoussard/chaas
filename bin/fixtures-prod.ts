c48c3e9190db51c6821dea2bd0e163515db12d9e
Théo Sallou <theo.sallou@akeneo.com>
2025-11-24 18:31:45 +0100

    add a way to convert prod data to new app data

---
+++ bin/fixtures-prod.ts
@@ -0,0 +1,24 @@
+import {exit} from 'process';
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+import {createFirebaseApp, Env} from '../src/utils/firebase';
+import {
+  createUser,
+  Dataset,
+  loadAccounts,
+  loadItems,
+  loadTransactions,
+  updateAccountActivity,
+} from '../src/utils/fixtures';
+
+const app = createFirebaseApp(Env.DEV);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+await createUser(auth, 'michel@chaquip.com', 'michel');
+await loadAccounts(firestore, Dataset.PROD);
+await loadItems(firestore, Dataset.PROD);
+await loadTransactions(firestore, Dataset.PROD);
+await updateAccountActivity(firestore);
+
+exit(0);
