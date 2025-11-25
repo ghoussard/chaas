2b0753d333822a58560bbb1b91f5a79cb982db86
Théo Sallou <theo.sallou@akeneo.com>
2025-11-25 09:42:35 +0100

    Fix app not loading on first login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import {useAuth} from './hooks';
 import {AccountGrid, Login} from './pages';
+import {StoreContext, createStoreContextValue, ItemsProvider} from './contexts';
+import {getFirestore} from 'firebase/firestore';
 
 export const App = () => {
   const {isLoggedIn} = useAuth();
 
-  return isLoggedIn ? <AccountGrid /> : <Login />;
+  if (!isLoggedIn) {
+    return <Login />;
+  }
+
+  const firestore = getFirestore();
+  const storeContextValue = createStoreContextValue(firestore);
+
+  return (
+    <ItemsProvider firestore={firestore}>
+      <StoreContext.Provider value={storeContextValue}>
+        <AccountGrid />
+      </StoreContext.Provider>
+    </ItemsProvider>
+  );
 };
